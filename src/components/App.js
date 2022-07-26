3a300500ab66c090b83f0e717a50f3fccbc6f722
George Duncan <gtduncan@usc.edu>
2022-07-26 11:29:54 -0400

    commit tree ef0ee949665dbffb5cc42dd9e48b6bde96da942f

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,27 @@
+import { useState, useEffect } from 'react'
+
 // create your App component here
+const App = () => 
+{
+    const [dog, setDog] = useState('')
+    useEffect(() =>
+    {
+        const getDog = fetch("https://dog.ceo/api/breeds/image/random")
+        .then((response) => response.json())
+        .then((data) => {
+            setDog(data.message)
+        })
+    }, [])
+    if(!dog)
+    {
+        return <p>Loading...</p>
+    }
+    else{
+        return(<div>
+            <img src={dog} alt="A Random Dog"></img>
+        </div>) 
+    }
+    
+}
+
+export default App
